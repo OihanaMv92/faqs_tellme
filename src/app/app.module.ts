ce0479b9f93cf9e3dc4ff437a24d20970b0ac9d2
Oihana Mendez <Oihanamv92@gmail.com>
2022-10-24 23:25:03 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BodyComponent,
+    FooterComponent,
+ 
   ],
   imports: [
     BrowserModule
